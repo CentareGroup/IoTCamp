fb3267d611d75294a530ab5ed3fe7ddb09318c1a
scherrer <jacob.scherrer@centare.com>
2018-03-13 16:21:03 -0500

    changes

--- src/settings.example.h
+++ src/settings.example.h
@@ -6,3 +6,6 @@ const int mqtt_port = 8883;
 
 const char* mqtt_client_id = "...";
 const char* mqtt_publish_topic = "IoTCamp/TEST";
+
+const char* certificate_path = "/certificate.der";
+const char* private_key_path = "/private.der";
