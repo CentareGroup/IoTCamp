3df363f90173f6e90e2ebcccf9896f66dcd1ff3b
scherrer <scherrer@gmail.com>
2018-03-13 11:57:05 -0500

    Inital commit

---
+++ src/settings.example.h
@@ -0,0 +1,8 @@
+const char* ssid = "...";
+const char* password = "...";
+
+const char* mqtt_server = "...";
+const int mqtt_port = 8883;
+
+const char* mqtt_client_id = "...";
+const char* mqtt_publish_topic = "IoTCamp/TEST";
